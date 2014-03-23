f5d983f98d1296c25dd54f08119712691559cf24
Arthur200000 <apple@192.168.0.3>
2014-03-23 18:48:51 +0800

    Init

--- 2048.lua
+++ 2048.lua
@@ -1,9 +1,13 @@
--- platform.apilevel = '2.0'     
+-- 2048 for TI-Nspire, NOT WORKING YET
+-- Original JavaScript Source: https://github.com/gabrielecirulli/2048/tree/master/js
+--[[ (Long Copyrights) --]]
+-- platform.apilevel = '2.0'   -- TI-Lua API version, maybe I don't need so many TI APIs.
 
 size = 4
 score = 0
 player = 
 powers = {2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}
+over = False
 
 Tile = {}
 for row=1,size do
@@ -13,15 +17,33 @@ for row=1,size do
     end
 end
 
+functi
+
+function on.arrowRight()
+    if not over then
+        cells = this.availableCells(right)
+        this.push(right)
+        this.merge(right)
+        this.push(right) --Can I avoid Pushing twice?
+    end
+end
+
+availableCells = function(direcion)
+    --TODO: Let pichu the OIer do this
+end
+
 push = function(direction)
-    
+    -- TODO: Let pichu the OIer do this
 end
 
 randomAvailableCell = function()
-    
+    -- Get this before pushing 
+    -- Choose from var.recallAt(cells, RandomFromOne_To_ElementCountOfCells)
+    -- TODO: Let pichu the OIer do this
 end
 
 addRandomTile = function()
+    -- Do after pushing
     cell = this.randomAvailableCell --{x, y}
     block = math.random(1,2)
     Tile [var.recallAt("cell", 1) ][var.recallAt("cell", 2)] = var.recall(block)
