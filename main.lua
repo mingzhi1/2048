950034c9a2eebb0fcfb8ec51c286f40d0da1eae1
pan.mingzhi <pmz1321@163.com>
2014-03-25 02:50:26 +0800

    add something

--- main.lua
+++ main.lua
@@ -1,5 +1,7 @@
 
-girds = {
+grids = {
+	cells = nil,
+	size = 0
 }
 
 Tile = {
@@ -8,18 +10,68 @@ Tile = {
     value = 0,
 }
 
-function Tile:new()
+function Tile:new(cells)
     math.randomseed(os.time())
-    x     = math.random(1,4)
-    y     = math.random(1,4)
-    value = 2
+    local idx = math.random(1,table.getn(cells))
+    self.x = cells[idx].x
+    self.y = cells[idx].y
+    if math.random(1,10) == 9 then
+    	self.value = 4
+    else
+    	self.value = 2
+    end
 end
 
-function Tile:updatePosition(position)
-    x = position.x;
-    y = position.y;
+function Tile:update(position, value)
+    self.x = position.x
+    self.y = position.y
+    self.value = self.value + value
 end
 
-function girds:merge()
+function grids:new(o)
+	o = o or {}
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
 
+function grids:empty()
+	local cells = {}
+
+	for x = 0, self.size do
+		cells[x] = {}
+		for y = 0, self.size do
+			cells[x][y] = 0
+		end
+	end
+
+	self.cells = cells
+end
+
+function grids:merge()
+
+end
+
+function grids:move(direction)
+	-- body
+end
+
+function grids:availables()
+	local cells = {}
+	for i = 0, self.size do
+		for j = 0, self.size do
+			if self.cells[i][j] == nil then
+				table.insert(cells, {x = i, y = j})
+			end
+		end
+	end
+	return cells
+end
+
+function grids:insert(tile)
+	self.cells[tile.x][tile.y] = tile
+end
+
+function grids:remove(tile)
+	self.cells[tile.x][tile.y] = nil
 end
